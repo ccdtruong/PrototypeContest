83fad65560012885e231b41b7e61d10230906503
Kỳ Vũ <vucaoky99@gmail.com>
2022-07-31 16:31:38 +0700

    Add players collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,15 +15,18 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private Transform m_groundCheckCollider;
+    
     [SerializeField]
     LayerMask m_groundlayerMask;
 
-    private float horizontal;
+    [SerializeField]
+    ParticleSystem m_sparksParticles;
 
+    private float horizontal;
     private bool m_IsSelected;
     private bool m_isJumping;
     private bool m_isGrounded;
-
+    private bool m_isSparking;
     private Vector3 m_screenBounds;
 
     private Animator m_animator;
@@ -49,6 +52,8 @@ public class Player : MonoBehaviour
         {
             Jump();
         }
+
+        //Debug.Log("isJumping = " + m_isJumping);
     }
 
     // Update is called once per frame
@@ -56,11 +61,16 @@ public class Player : MonoBehaviour
     {
         if (!m_IsSelected) return;
         GroundCheck();
-        Debug.Log("isGround" + m_isGrounded);
+        Debug.Log("isGround = " + m_isGrounded);
         if (m_isGrounded)
         {
             m_animator.SetBool("isJumping", false);
             m_isJumping = false;
+            Debug.Log("isJumpingFalse");
+        } else
+        {
+            Debug.Log("isJumpingTrue " + m_isJumping);
+            if (m_isJumping) m_animator.SetBool("isJumping", true);
         }
         Move(horizontal);
     }
@@ -82,7 +92,6 @@ public class Player : MonoBehaviour
             m_animator.SetBool("isJumping", true);
             m_rigidbody2D.velocity = Vector2.up * m_jumpForce;
             m_isJumping = true;
-            m_isGrounded = false;
         }
     }
 
@@ -98,7 +107,7 @@ public class Player : MonoBehaviour
     {
         m_isGrounded = false;
         Collider2D[] colliders = Physics2D.OverlapCircleAll(m_groundCheckCollider.position, 0.1f, m_groundlayerMask);
-        Debug.Log("colliders.Length = " + colliders.Length);
+        //Debug.Log("colliders.Length = " + colliders.Length);
         if (colliders.Length > 0)
         {
             m_isGrounded = true;
@@ -107,7 +116,7 @@ public class Player : MonoBehaviour
 
     public void Move(float dir)
     {
-        if (dir == 0)
+        if (dir == 0 || m_isSparking)
         {
             m_animator.SetBool("isWalking", false);
             return;
@@ -117,16 +126,31 @@ public class Player : MonoBehaviour
         {
             Flip();
         }
-        m_animator.SetBool("isWalking", true);
+        if (m_isGrounded)
+        {
+            m_animator.SetBool("isWalking", true);
+        }
         float speed = m_speed;
         if (!m_isGrounded)
         {
             speed = m_jumpSpeed;
         }
-        Debug.Log("speed = " + speed);
         float xVal = dir * speed * Time.fixedDeltaTime;
         Vector2 targetVelocity = new Vector2(xVal, m_rigidbody2D.velocity.y);
         m_rigidbody2D.velocity = targetVelocity;
+        //Debug.Log("speed " + name + " = " + m_rigidbody2D.velocity.ToString());
+    }
+
+    void SparkBurst()
+    {
+        m_sparksParticles.Emit(20);
+    }
+
+    void SparkStop()
+    {
+        //Debug.Log("End spark");
+        m_animator.SetBool("isFocus", false);
+        m_isSparking = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -137,7 +161,7 @@ public class Player : MonoBehaviour
             //controller.OpenGate();
             controller.SetGateState(true);
             collision.gameObject.SetActive(false);
-            Debug.Log("Get Key");
+            //Debug.Log("Get Key");
         }
         else if (collision.gameObject.tag == "Gate")
         {
@@ -146,4 +170,24 @@ public class Player : MonoBehaviour
         }
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector2 posA = transform.position;
+            Vector2 posB = collision.gameObject.transform.position;
+            Vector2 collisionVector = posB - posA;
+            m_rigidbody2D.velocity = - collisionVector * m_jumpForce;
+            Debug.Log("collisionVecter " + name + " = " + m_rigidbody2D.velocity.ToString());
+            if ((collisionVector.x < 0 && m_facingRight)
+                || (collisionVector.x > 0 && !m_facingRight))
+            {
+                Flip();
+            }
+            SparkBurst();
+            m_isSparking = true;
+            m_animator.SetBool("isFocus", true);
+            Invoke("SparkStop", 0.3f);
+        }
+    }
 }
