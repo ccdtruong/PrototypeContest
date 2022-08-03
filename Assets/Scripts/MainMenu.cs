ba08e9fdde8723e6e2d55b1a93ab24c9ed7a021c
ccdtruong <ccdtruong@gmail.com>
2022-08-04 00:10:36 +0700

    Update UI

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+}
